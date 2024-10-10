d9368b26978258afe89043a31f2e4087fd69d239
alever_sn <759636538@qq.com>
2024-10-10 17:06:08 +0800

    [fix] Fix inference_with_data_path.

--- main/evaluation/inferences.py
+++ main/evaluation/inferences.py
@@ -10,35 +10,6 @@ def get_data_present(present_path):
     data_present = json.loads(present_json)
     return data_present
 
-def inference_with_data_path(data_path, batcher, save_path, batch_size=1, skip=-1, eval_mode='dev'):
-    '''
-    - `params`: `data_path`: str, data_path is the dataset source, which is defined in `./data/present.json`.
-    - `params`: `batcher`: function, batcher is the function to batch the data.
-    - `params`: `save_path`: str, save_path is the path to save the inference result.
-    - `params`: `batch_size`: int, batch_size is the batch size of the inference.
-    - `params`: `skip`: int, skip is the number of data to skip.
-    - `params`: `eval_mode`: str, eval_mode is the mode of the inference, which is defined in `./data/present.json`.
-    - `return`: `result`: list, result is the inference result.
-    '''
-    data_present = get_data_present('./data/present.json')
-    file_name = data_present[data_path][eval_mode]
-    with open(file_name, 'r') as f:
-        lines = f.readlines()
-    data = [json.loads(line) for line in lines]
-    format_data = []
-    for item in data:
-        item = item['conversations'] if 'conversations' in item else item
-        assert len(item) > 1
-        if len(item) >= 2:
-            query = item[-2]['content']
-            history = item[:-2]
-        else:
-            query = item[-1]['content']
-            history = []
-        format_data.append({'query': query, 'history': history})
-            
-    return inference_with_data(format_data, batcher, save_path, batch_size, skip)
-
 def inference_with_data(data, batcher, save_path=None, batch_size=1, skip=-1, yield_output=False):
     '''
     - `params`: `data`: list, data is the dataset to inference.
@@ -93,4 +64,34 @@ def inference_with_data(data, batcher, save_path=None, batch_size=1, skip=-1, yi
                 }
                 yield format_output
         
-    return result
+    yield result
+
+def inference_with_data_path(data_path, batcher, save_path, batch_size=1, skip=-1, eval_mode='dev'):
+    '''
+    - `params`: `data_path`: str, data_path is the dataset source, which is defined in `./data/present.json`.
+    - `params`: `batcher`: function, batcher is the function to batch the data.
+    - `params`: `save_path`: str, save_path is the path to save the inference result.
+    - `params`: `batch_size`: int, batch_size is the batch size of the inference.
+    - `params`: `skip`: int, skip is the number of data to skip.
+    - `params`: `eval_mode`: str, eval_mode is the mode of the inference, which is defined in `./data/present.json`.
+    - `return`: `result`: list, result is the inference result.
+    '''
+    data_present = get_data_present('./data/present.json')
+    file_name = data_present[data_path][eval_mode]
+    with open(file_name, 'r') as f:
+        lines = f.readlines()
+    data = [json.loads(line) for line in lines]
+    format_data = []
+    for item in data:
+        item = item['conversations'] if 'conversations' in item else item
+        assert len(item) > 1
+        if len(item) >= 2:
+            query = item[-2]['content']
+            history = item[:-2]
+        else:
+            query = item[-1]['content']
+            history = []
+        format_data.append({'query': query, 'history': history})
+            
+    for result in inference_with_data(data=format_data, batcher=batcher, save_path=save_path, batch_size=batch_size, skip=skip):
+        return result
